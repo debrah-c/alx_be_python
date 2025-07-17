bbf2443b86c0eea24609863570ecf626fd81bf53
Deborah <yegondebby19@gmail.com>
2025-07-17 09:45:34 +0300

    Update simple_interest.py

--- python_introduction/simple_interest.py
+++ python_introduction/simple_interest.py
@@ -3,10 +3,10 @@ Simple Interest = "I"
 Principal = "P" 
 Rate = "R"
 Time = "T"
-I = "P * R * T"  #This is the formula of finding I which is the interest.
 #Assign values to the variables
 P = 1000 ($)
 R = 0.05 (%)
 T = 3 ("years")
+I = "P * R * T"  #This is the formula of finding I which is the interest.
 #Calculate the value of I using the formula: I = P * R * T
 print("The simple interest is:", P * R * T)

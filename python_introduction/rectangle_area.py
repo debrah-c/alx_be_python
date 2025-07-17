c57f018d04bad611d3171a8c877043ab6020d7a7
Deborah <yegondebby19@gmail.com>
2025-07-17 13:56:53 +0300

    Update rectangle_area.py

--- python_introduction/rectangle_area.py
+++ python_introduction/rectangle_area.py
@@ -1,6 +1,6 @@
 #First define the variables.
 Length = 10
 Width = 5
-#Area = Length * Width
+Area = Length * Width
 #Calculate the area using the formula (Area = Length * Width)
 print("The area of the rectangle is:", Length * Width)

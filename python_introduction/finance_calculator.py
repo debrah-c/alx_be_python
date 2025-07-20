78cf5bbd78b308e5a6ec6d6c9c53138929524bc4
Deborah <yegondebby19@gmail.com>
2025-07-20 23:33:57 +0300

    Add files via upload

---
+++ python_introduction/finance_calculator.py
@@ -0,0 +1,11 @@
+#First define the variables.
+monthly_income = input("Enter your monthly income.")
+monthly_income = 5000
+print("monthly_income = 5000")
+monthly_expenses = input("Enter your monthly expenses")
+monthly_expenses = 4000
+print("monthly_expenses = 4000")
+#Calculate monthly savings using the formula (monthly_savings = monthly_income - monthly_expenses)
+print("monthly_savings = monthly_income - monthly_expenses")
+#calculate the projected savings using the formula (projected_savings = monthly_savings * 12 + (monthly_savings * 12 * 0.05))
+print("projected_savings = monthly_savings * 12 + (monthly_savings * 12 * 0.05)")

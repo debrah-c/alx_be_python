cc49e9bd57cb4fed473e2cbd0d009c27f9042ba9
Debrah <yegondebby19@gmail.com>
2025-07-24 12:51:22 +0300

    Initial commit with existing files

---
+++ weather_advice.py
@@ -0,0 +1,7 @@
+current_weather = input("What is the weather today?" , ("sunny/rainy/cold"))
+if "sunny":
+    print("Wear a t-shirt and sunglasses.")
+    if "rainy":
+        print("Don't forget your umbrella and a raincoat.")
+        if "cold":
+            print("Make sure to wear a warm coat and a scarf.")

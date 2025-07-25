fb59aedf63d99f607c559a7f908d70e6ee72515d
Debrah <yegondebby19@gmail.com>
2025-07-25 15:08:52 +0300

    Add control-flow as regular directory

---
+++ control-flow/match_case_calculator.py
@@ -0,0 +1,22 @@
+num1 = float(input("Enter the first number:") )
+num2 = float(input("Enter the second number:") )
+operation = input("Choose operation +, -, *,/:")
+
+match operation:
+    case "+":
+        print(f"{num1} + {num2} = {num1 + num2}")
+    case "-":
+        print(f"{num1} - {num2} = {num1 - num2}")
+    case "*":
+        print(f"{num1} * {num2} = {num1 * num2}")
+    case "/":
+        if num2 == 0:
+          print ("Cannot divide by 0")
+        print(f"{num1 / num2} = {num1 / num2}")
+    case _:
+        print("Invalid operation")
+    
+
+        
+        
+

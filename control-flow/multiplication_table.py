c6df277edbbac6674bd8ac14ed14a8aa77e68b94
Deborah <yegondebby19@gmail.com>
2025-07-30 12:32:45 +0300

    Add files via upload

---
+++ control-flow/multiplication_table.py
@@ -0,0 +1,8 @@
+number = input("Enter a number to see its multiplication table:")
+x = "number"
+y = "current number"
+z = " x * y"
+for y in range(1, 11) :          #1 to 10
+    print (f"{x} *{y} = {z}")    #x= the user's number,y= the current number, z = the product
+
+           

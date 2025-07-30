006c1f5c1e3efb4018368200ec42f76a11f9878f
Deborah <yegondebby19@gmail.com>
2025-07-30 13:32:00 +0300

    Update pattern_drawing.py

--- control-flow/pattern_drawing.py
+++ control-flow/pattern_drawing.py
@@ -1,4 +1,4 @@
-size = int(input("Enter the size of the pattern"))  #should be positive integer
+size = int(input("Enter the size of the pattern:"))  #should be positive integer
 i = 0
 while i < size:
     for j in range(size):

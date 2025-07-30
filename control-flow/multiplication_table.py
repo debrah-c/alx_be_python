284bb0d7b91da809289998bc0ab07b2769610ead
Deborah <yegondebby19@gmail.com>
2025-07-30 12:47:37 +0300

    Update multiplication_table.py

--- control-flow/multiplication_table.py
+++ control-flow/multiplication_table.py
@@ -1,9 +1,5 @@
 number = input("Enter a number to see its multiplication table:")
-x = "number"
-y = "current number"
-z = " x * y"
-print(z = x * y)
 for y in range(1, 11) :          #1 to 10
-    print (f"{x} *{y} = {z}")    #x= the user's number,y= the current number, z = the product
+    print (f"{number} *{y} = {number * y}")    #x= the user's number,y= the current number, z = the product
 
            
